f26dc8cf012d07fb1d0d5ed8516dc9bb7d504d41
aong <yxwzaxns@gmail.com>
2015-06-02 16:26:08 +0800

    first add

---
+++ README.md
@@ -0,0 +1,1 @@
+# this is a demo of hello world use go language
---
+++ demo.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+)
+
+func main() {
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintln(w,"Hello, world!")
+  })
+  if err := http.ListenAndServe(":80", nil); err != nil {
+    panic(err)
+  }
+}
