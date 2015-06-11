5f72f5836133574fbabd8f862a83407bf1243bca
aong <yxwzaxns@gmail.com>
2015-06-11 21:00:38 +0800

    alter demo.go

--- demo.go
+++ demo.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintln(w,"Hello, world!")
+    fmt.Fprintln(w,"Hello, world!,this is a demo of GO")
   })
   if err := http.ListenAndServe(":80", nil); err != nil {
     panic(err)
